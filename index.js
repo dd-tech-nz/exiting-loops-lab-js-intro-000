dea1ad7d028d7252636f17aff3e096dfab15117a
David Dumolo <ddumolo@hotmail.com>
2020-03-14 21:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function findBy(array, findFn) {
 }
 
 function findFn(array) {
-  for (let i = 0; i < array.length) {
+  for (let i = 0; i < array.length; i++) {
     if (array[i] === 'c') {
       return true
     }
