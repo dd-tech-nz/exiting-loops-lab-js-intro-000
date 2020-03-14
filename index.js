6990af5138f28826312c80b60f736a1bfefca427
David Dumolo <ddumolo@hotmail.com>
2020-03-14 21:38:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === stopValue){
-      break
-    } else {
-      array[i] = changeValue
-    }
+    array[i] = stopValue ? break : array[i] = changeValue;
 
   }
+  return array
 }
