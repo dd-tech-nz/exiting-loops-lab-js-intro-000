00a4bd0028a5784670d98ba5595aa80365b00ccb
David Dumolo <ddumolo@hotmail.com>
2020-03-14 22:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   let element = findFn
-  
+
   for (let i = 0; i < array.length; i++)
   if (array[i] === element ) {
     return null
